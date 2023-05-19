d6b3fb8d111e82c399ac71376ee458aec6ad228c
yuvraj-g <yuvrajwebdev@gmail.com>
2023-05-19 21:25:16 +0530

    changed the google logo image directory src

--- src/components/SidebarLeft/SidebarLeft.jsx
+++ src/components/SidebarLeft/SidebarLeft.jsx
@@ -20,7 +20,7 @@ const SidebarLeft = () => {
     <div className={sidebarVisibility ? 'absolute left-0 top-0 bg-gray-50 flex flex-col px-6 py-4 w-72 shadow-lg shadow-gray-500 min-h-screen transition-all' : 'absolute -left-72 top-0 bg-gray-50 flex flex-col px-6 py-4 w-72 shadow-lg shadow-gray-500 min-h-screen transition-all'} ref={sidebarRef}>
       <div className='flex gap-2 items-center cursor-pointer'>
         <div className='w-[4.5rem] h-8 flex justify-center items-center -mb-1'>
-          <img src="../../src/assets/img-icons/google.png" alt="google" className='w-fit object-cover bg-transparent' />
+          <img src="/src/assets/img-icons/google.png" alt="google" className='w-fit object-cover bg-transparent' />
         </div>
         <h3 className='font-normal text-[1.35rem] text-gray-600 ml-0'>Docs</h3>
       </div>
